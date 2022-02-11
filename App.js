1978d68ec4f5b6a6c259007c39f9b65f074f27fb
hlmgnc <61838067+hlmgnc@users.noreply.github.com>
2022-02-11 17:54:28 +0100

    Add clear todos functionality

--- App.js
+++ App.js
@@ -21,42 +21,53 @@ export default function App() {
     setLabel("");
   };
 
+  const clearAll = () => {
+    setTodos((oldTodos) => []);
+  };
+
   return (
     <View style={styles.container}>
       <Text>TODO APP!</Text>
-      <SafeAreaView style={styles.inputContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder="add your todo"
-          onChangeText={setLabel}
-          value={label}
-        />
-        <Button
-          title="ADD"
-          color="green"
-          height="20"
-          paddingTop="20"
-          // style={styles.buttonStyle}
-          onPress={addTodo}
-        />
-      </SafeAreaView>
-
-      <FlatList
-        data={todos}
-        renderItem={({ item }) => (
-          <Text>
-            <BouncyCheckbox
-              size={25}
-              fillColor="red"
-              unfillColor="#FFFFFF"
-              iconStyle={{ borderColor: "red" }}
-              onPress={() => {}}
-            />
-            {item}
-          </Text>
-        )}
-        keyExtractor={(item, index) => index}
-      />
+      <View style={styles.inputContainer}>
+        <View>
+          <TextInput
+            style={styles.input}
+            placeholder="add your todo"
+            onChangeText={setLabel}
+            value={label}
+          />
+          <Button
+            title="ADD"
+            color="green"
+            style={styles.buttonStyle}
+            onPress={addTodo}
+          />
+        </View>
+        <View style={{ height: 600 }}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <Text>
+                <BouncyCheckbox
+                  size={25}
+                  fillColor="red"
+                  unfillColor="#FFFFFF"
+                  iconStyle={{ borderColor: "red" }}
+                  onPress={() => {}}
+                />
+                {item}
+              </Text>
+            )}
+            keyExtractor={(item, index) => index}
+          />
+          <Button
+            title="CLEAR ALL"
+            color="red"
+            // style={styles.clearButton}
+            onPress={clearAll}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -65,14 +76,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
+    marginTop: 100,
   },
-  inputContainer: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
+  // inputContainer: {
+  //   display: "flex",
+  //   flexDirection: "column",
+  //   justifyContent: "space-between",
+  // },
   input: {
     height: 40,
     width: 200,
@@ -80,5 +93,10 @@ const styles = StyleSheet.create({
     borderWidth: 0.5,
     padding: 10,
   },
-  buttonStyle: {},
+  buttonStyle: {
+    flex: 0.3,
+    backgroundColor: "beige",
+    borderWidth: 5,
+  },
+  clearButton: {},
 });
